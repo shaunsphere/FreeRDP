cb48d1b4d7746b1df72a9cd58fd583356ae19b9d
Armin Novak <armin.novak@thincast.com>
2016-03-02 14:39:36 +0100

    Unified RDPGFX_RECT16 and RECTANGLE_16

--- client/X11/xf_gfx.c
+++ client/X11/xf_gfx.c
@@ -813,7 +813,7 @@ UINT xf_SolidFill(RdpgfxClientContext* context, RDPGFX_SOLID_FILL_PDU* solidFill
 	UINT32 color;
 	BYTE a, r, g, b;
 	int nWidth, nHeight;
-	RDPGFX_RECT16* rect;
+	RECTANGLE_16* rect;
 	xfGfxSurface* surface;
 	RECTANGLE_16 invalidRect;
 	xfContext* xfc = (xfContext*) context->custom;
@@ -864,7 +864,7 @@ UINT xf_SurfaceToSurface(RdpgfxClientContext* context, RDPGFX_SURFACE_TO_SURFACE
 	UINT16 index;
 	BOOL sameSurface;
 	int nWidth, nHeight;
-	RDPGFX_RECT16* rectSrc;
+	RECTANGLE_16* rectSrc;
 	RDPGFX_POINT16* destPt;
 	RECTANGLE_16 invalidRect;
 	xfGfxSurface* surfaceSrc;
@@ -927,7 +927,7 @@ UINT xf_SurfaceToSurface(RdpgfxClientContext* context, RDPGFX_SURFACE_TO_SURFACE
 UINT xf_SurfaceToCache(RdpgfxClientContext* context, RDPGFX_SURFACE_TO_CACHE_PDU* surfaceToCache)
 {
 	size_t size;
-	RDPGFX_RECT16* rect;
+	RECTANGLE_16* rect;
 	xfGfxSurface* surface;
 	xfGfxCacheEntry* cacheEntry;
 	xfContext* xfc = (xfContext*) context->custom;
