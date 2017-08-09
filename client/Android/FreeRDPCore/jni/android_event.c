a1bfce1430b47ec8008201e6664bfaf41fcede2d
shaunsphere <nxshaun@gmail.com>
2017-08-09 14:48:51 -0400

    Update android_event.c

--- client/Android/FreeRDPCore/jni/android_event.c
+++ client/Android/FreeRDPCore/jni/android_event.c
@@ -9,7 +9,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
